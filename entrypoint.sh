569a0dc0bb37215d5c49d9e7a8ff69c4908a2557
Jose Zambudio Bernabeu <jose@aurestic.es>
2021-10-25 14:57:18 +0200

    [ADD] Allow empty commits

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -eu
 
 # Set up .netrc file with GitHub credentials
 git_setup ( ) {
-  cat <<- EOF > $HOME/.netrc
+  cat - EOF > $HOME/.netrc
         machine github.com
         login $GITHUB_ACTOR
         password $GITHUB_TOKEN
@@ -20,28 +20,30 @@ EOF
 echo "###################"
 echo "Tagging Parameters"
 echo "###################"
-echo "flag_branch: <<${INPUT_FLAG_BRANCH}>>"
-echo "message: <<${INPUT_MESSAGE}>>"
-echo "prev_tag: <<${INPUT_PREV_TAG}>>"
-echo "update_odoo_module_version: <<${INPUT_UPDATE_ODOO_MODULE_VERSION}>>"
+echo "flag_branch: ${INPUT_FLAG_BRANCH}"
+echo "message: ${INPUT_MESSAGE}"
+echo "prev_tag: ${INPUT_PREV_TAG}"
+echo "update_odoo_module_version: ${INPUT_UPDATE_ODOO_MODULE_VERSION}"
 echo "###################"
+echo ""
+echo "Start process..."
 
-echo "Setting up git machine..."
+echo "1) Setting up git machine..."
 git_setup
 
-echo "Updating repository tags..."
-git fetch origin --tags
+echo "2) Updating repository tags..."
+git fetch origin --tags --quiet
 
 last_tag=""
 if [ "${INPUT_FLAG_BRANCH}" = true ];then
     branch=$(git rev-parse --abbrev-ref HEAD)
-    echo "branch: <<${branch}>>";
+    echo "branch: ${branch}";
 
     last_tag=`git describe --tags $(git rev-list --tags) --always|egrep "${INPUT_PREV_TAG}${branch}\.[0-9]*\.[0-9]*$"|sort -V -r|head -n 1`
-    echo "Last tag: <<${last_tag}>>";
+    echo "Last tag: ${last_tag}";
 else
     last_tag=`git describe --tags $(git rev-list --tags --max-count=1)`
-    echo "Last tag: <<${last_tag}>>";
+    echo "Last tag: ${last_tag}";
 fi
 
 
@@ -51,16 +53,16 @@ if [ -z "${last_tag}" ];then
     else
         last_tag="${INPUT_PREV_TAG}0.1.0";
     fi
-    echo "Default Last tag: <<${last_tag}>>";
+    echo "Default Last tag: ${last_tag}";
 fi
 
 next_tag="${last_tag%.*}.$((${last_tag##*.}+1))"
-echo "Next tag: <<${next_tag}>>";
+echo "3) Next tag: ${next_tag}";
 
 
 if [ "${INPUT_UPDATE_ODOO_MODULE_VERSION}" = true ];then
-    echo "Upload tag for Odoo module...";
-    git checkout "${GITHUB_SHA}";
+    echo "4) Upload tag for Odoo module...";
+    git checkout --quiet "${GITHUB_SHA}";
 
     for file in '__openerp__.py' '__manifest__.py';do
         if [ ! -f "${file}" ];then
@@ -74,16 +76,15 @@ if [ "${INPUT_UPDATE_ODOO_MODULE_VERSION}" = true ];then
         git add ${file}
     done
 
-    git commit -m "${INPUT_MESSAGE}"
+    git commit --allow-empty -m "${INPUT_MESSAGE}"
     tag_commit=`git rev-parse --verify HEAD`
-    echo "Forcing tag update..."
+    echo "5) Forcing tag update..."
     git tag ${next_tag}
-    echo "Forcing tag push..."
+    echo "6) Forcing tag push..."
     git push --tags
 else
-    echo "Forcing tag update..."
+    echo "4) Forcing tag update..."
     git tag -a ${next_tag} -m "${INPUT_MESSAGE}" "${GITHUB_SHA}" -f
-    echo "Forcing tag push..."
+    echo "5) Forcing tag push..."
     git push --tags -f
 fi
-
